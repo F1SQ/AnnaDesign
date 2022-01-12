1c0619fdd0a7ae3c532e452977f1ea9eae2a2a8f
denis <ridchenkodenis@gmail.com>
2022-01-12 07:54:20 +0200

    change: create popup, change media

--- js/script.js
+++ js/script.js
@@ -44,12 +44,6 @@ if (menuLinks.length > 0) {
     });
 
 
-AOS.init({
-  startEvent: 'DOMContentLoaded',
-  once: true,
-  disable: function() { var maxWidth = 992; return window.innerWidth < maxWidth; }
-});
-
 
 const menuButton = document.querySelector('.header-button');
 const mobileMenu = document.querySelector('.hidden-menu');
@@ -63,14 +57,36 @@ const hiddenLinks = document.querySelectorAll('.hidden-link');
 if (hiddenLinks.length > 0) {
   hiddenLinks.forEach(hiddenLink => {
     hiddenLink.addEventListener("click", function(){
-      console.log('click')     
+      console.log('click');     
       mobileMenu.classList.remove('hidden-menu--visible');
       menuButton.classList.remove('_active');
     }); 
   });
   }
 
+const popupButton = document.querySelector('.popup-button');
+const popup = document.querySelector('.contacts-popup');
 
+popupButton.addEventListener('click', () => {  
+  popup.classList.add('active');
+  copyToClipboard();
+
+});
+
+popup.addEventListener('animationend', () => {
+  popup.classList.remove('active');
+});
+
+function copyToClipboard() {
+  const textarea = document.createElement('textarea');
+  textarea.setAttribute('readonly', '');
+  textarea.style.position= 'absolute';
+  textarea.value = ('https://www.instagram.com/anna.ndesign/');
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
 
 });
 
